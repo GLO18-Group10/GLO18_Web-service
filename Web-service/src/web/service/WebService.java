0c8852c3dcb3afde20121161f9e19038d06fc495
Robinlp <Robinlp@users.noreply.github.com>
2018-10-12 12:12:13 +0200

    init

---
+++ Web-service/src/web/service/WebService.java
@@ -0,0 +1,16 @@
+package web.service;
+
+/**
+ *
+ * @author Robin
+ */
+public class WebService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
