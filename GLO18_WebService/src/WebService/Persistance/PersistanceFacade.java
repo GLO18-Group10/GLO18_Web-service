52afea2e089526afbcfd46037023f724756255e6
Robinlp <Robinlp@users.noreply.github.com>
2018-10-24 15:56:51 +0200

    database manager implementation
    the initial setup for the DBManager

--- GLO18_WebService/src/WebService/Persistance/PersistanceFacade.java
+++ GLO18_WebService/src/WebService/Persistance/PersistanceFacade.java
@@ -12,5 +12,8 @@ import WebService.Acquaintance.iPersistance;
  * @author Jeppe Enevold
  */
 public class PersistanceFacade implements iPersistance {
+    DBManager dbm = new DBManager();
+    
+    
     
 }
