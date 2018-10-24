8bf4388b65db67a267451bcd14405120f8a67da1
Peterzxcvbnm <43370082+Peterzxcvbnm@users.noreply.github.com>
2018-10-24 13:08:25 +0200

    MessagerParser fromProtocol switch
    Added a switch to process messages

--- GLO18_WebService/src/WebService/Link/MessageParser.java
+++ GLO18_WebService/src/WebService/Link/MessageParser.java
@@ -10,5 +10,26 @@ package WebService.Link;
  * @author Peterzxcvbnm
  */
 public class MessageParser {
-    
+
+    public void fromProtocol(String message) {
+        String code = message.substring(0, 1); //Seperate the OPcode
+        switch (code) {
+            case "00":
+            case "01":
+            case "02":
+            case "03":
+            case "04":
+            case "05":
+            case "06":
+            case "07":
+            case "08":
+            case "09":
+            case "10":
+            case "11":
+            case "12":
+            case "18":
+            case "19":
+            default:
+        }
+    }
 }
