e82e75e8fed012265a55f6a1908b7122fddb710f
Robinlp <Robinlp@users.noreply.github.com>
2018-11-28 12:09:52 +0100

    removed unused code

--- GLO18_WebService/src/WebService/Logic/LogicFacade.java
+++ GLO18_WebService/src/WebService/Logic/LogicFacade.java
@@ -5,7 +5,6 @@
  */
 package WebService.Logic;
 
-import java.time.LocalDateTime;
 import WebService.Acquaintance.ILogic;
 import WebService.Acquaintance.IPersistence;
 
@@ -33,98 +32,4 @@ public class LogicFacade implements ILogic {
         }
         return "error";
     }
-
-//    @Override
-//    public String login(String ID, String password) {
-//        String test = persistence.login(ID, password);
-//        return test;
-//    }
-
-//    @Override
-//    public String logout() {
-//        return "true";
-//    }
-
-//    /**
-//     *
-//     * @param ID
-//     * @return CostumerInfo
-//     */
-//    @Override
-//    public String getCustomerInfo(String ID) {
-//        return persistence.getCustomerInfo(ID); //Do a query to get the info that cooreponds to the given id
-//    }
-
-//    @Override
-//    public String storeCustomerInfo(String ID, String name, String phoneNo, String address, String email) {
-//        return persistence.storeCustomerInfo(ID, name, phoneNo, address, email);
-//    }
-
-//    @Override
-//    public String getAccountBalance(String ID) {
-//        return persistence.getAccountBalance(ID);
-//    }
-
-//    @Override
-//    public String createCustomer(String ID, String name, String birthday, String phonenumber, String address, String email, String password) {
-//        return persistence.createCustomer(ID, name, birthday, phonenumber, address, email, password);
-//    }
-
-//    @Override
-//    public boolean doesAccountExist(String accountID) {
-//        return persistence.doesAccountExist(accountID);
-//    }
-
-//    @Override
-//    public void updateAccountBalance(String accountID, int amount) {
-//        persistence.updateAccountBalance(accountID, amount);
-//    }
-
-//    @Override
-//    public String saveTransfer(String fromAccount, String toAccount, int amount, String text, LocalDateTime date) {
-//        return persistence.saveTransfer(fromAccount, toAccount, amount, text, date);
-//    }
-
-//    @Override
-//    public String[] getAccountNos(String customerID) {
-//        int noOfAccounts = 0;
-//        String[] accountNos = new String[10];
-//
-//        if (noOfAccounts == 0) {
-//            String[] accounts = persistence.getAccountNos(customerID).split(";");
-//            if (!accounts[0].equals("")) {
-//                noOfAccounts = accounts.length;
-//                for (int i = 0; i < accounts.length; i++) {
-//                    accountNos[i] = accounts[i];
-//                }
-//            }
-//        }
-//        if (noOfAccounts == 0) {
-//            String[] i = {"Error; no accounts found"};
-//            return i;
-//        } else {
-//            return accountNos;
-//        }
-//    }
-
-//    @Override
-//    public String getTransactionHistory(String accountID) {
-//        return persistence.getTransactionHistory(accountID);
-//    }
-
-//    @Override
-//    public String getCustomerIDs() {
-//        return persistence.getCustomerIDs();
-//    }
-
-//    @Override
-//    public void openAccount(String ID) {
-//        persistence.openAccount(ID);
-//    }
-//
-//    @Override
-//    public void closeAccount(String ID) {
-//        persistence.closeAccount(ID);
-//    }
-
 }
