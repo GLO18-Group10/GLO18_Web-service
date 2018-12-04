2f8ea5d092e0fa024dfe147ba7f93d5d8bd8286a
Robinlp <Robinlp@users.noreply.github.com>
2018-12-04 23:29:51 +0100

    logging

--- GLO18_WebService/src/WebService/Logic/ServerProtocol.java
+++ GLO18_WebService/src/WebService/Logic/ServerProtocol.java
@@ -29,9 +29,9 @@ public class ServerProtocol {
                 String ID = data[1];
                 String password = data[2];
                 String response00 = persistence.login(ID, password);
-                if(response00.equals("true")){
+                if (response00.equals("true")) {
                     logger.logAction(ID, "Logged in");
-                }else {
+                } else {
                     logger.logAction(ID, "Failed to log in");
                 }
                 return response00;
@@ -39,12 +39,13 @@ public class ServerProtocol {
                 logger.logAction(data[1], "Get customer info");
                 return persistence.getCustomerInfo(data[1]);
             case "02":
+                logger.logAction(data[2], "Get account balance");
                 return persistence.getAccountBalance(data[1]);
             case "03":
                 String response03 = persistence.storeCustomerInfo(data[5], data[1], data[2], data[3], data[4]);
-                if(response03.equals("true")){
+                if (response03.equals("true")) {
                     logger.logAction(data[5], "Stored customer info");
-                }else {
+                } else {
                     logger.logAction(data[5], "Failed to store customer info");
                 }
                 return response03;
@@ -64,6 +65,7 @@ public class ServerProtocol {
                 }
             case "06":
                 String ID2 = data[1];
+                logger.logAction(data[2], "Get transaction history");
                 return persistence.getTransactionHistory(ID2);
             case "07":
                 String ID1 = data[1];
@@ -73,7 +75,15 @@ public class ServerProtocol {
                 String address = data[5];
                 String email = data[6];
                 String password1 = data[7];
-                return persistence.createCustomer(ID1, name, birthday, phonenumber, address, email, password1);
+                String adminID = data[8];
+
+                String response07 = persistence.createCustomer(ID1, name, birthday, phonenumber, address, email, password1);
+                if (response07.equalsIgnoreCase("true")) {
+                    logger.logAction(adminID, "Created customer with ID: " + ID1);
+                } else {
+                    logger.logAction(adminID, "failed to create user with ID: " + ID1);
+                }
+                return response07;
             case "08":
                 String answer = "";
                 String[] accountNos = persistence.getAccountNos(data[1]);
@@ -88,14 +98,17 @@ public class ServerProtocol {
                 try {
                     if (data[2].equals("1")) {
                         persistence.openAccount(data[1]);
+                        logger.logAction(data[3], "Opened customer account with ID: " + data[1]);
                     } else if (data[2].equals("0")) {
                         persistence.closeAccount(data[1]);
+                        logger.logAction(data[3], "Closed customer account with ID: " + data[1]);
                     }
                     return "complete";
                 } catch (Exception e) {
                     return "Error; could not open/close account";
                 }
             case "10":
+                logger.logAction(data[1], "Get customer IDs");
                 //removes all "C" from Customer IDS              
                 return persistence.getCustomerIDs().replace("C", "");
             case "11":
@@ -106,13 +119,14 @@ public class ServerProtocol {
                 if (persistence.login(data[1], data[2]).equals("true")) {
                     try {
                         persistence.updatePassword(data[1], data[3]);
-                        logger.logAction(data[1], "Changed password");
+                        logger.logAction(data[1], "Change password succeeded");
                         return "true";
                     } catch (Exception e) {
                         System.out.println("Error; serverHandler; updatePassword");
                         return "Error; Unexpected SQL error";
                     }
                 } else {
+                    logger.logAction(data[1], "Change password failed");
                     return "Incorrect password";
                 }
             case "14":
@@ -126,9 +140,9 @@ public class ServerProtocol {
                 ID = data[1];
                 password = data[2];
                 String response16 = persistence.login(ID, password);
-                if(response16.equalsIgnoreCase("true")){
-                  logger.logAction(ID, "Password check: correct");  
-                }else{
+                if (response16.equalsIgnoreCase("true")) {
+                    logger.logAction(ID, "Password check: correct");
+                } else {
                     logger.logAction(ID, "password check: wrong");
                 }
                 return response16;
